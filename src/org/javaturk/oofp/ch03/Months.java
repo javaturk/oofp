d07e3a0d34e6f82cdc7593209262105983964ac5
javaturk <akin@javaturk.org>
2020-11-28 00:43:08 +0300

    Add files via upload

---
+++ src/org/javaturk/oofp/ch03/Months.java
@@ -0,0 +1,13 @@
+/* Ch 10
+ * Months.java
+ */
+
+package org.javaturk.oofp.ch03;
+
+public interface Months {
+    public static int
+    JANUARY = 1, FEBRUARY = 2, MARCH = 3,
+    APRIL = 4, MAY = 5, JUNE = 6, JULY = 7,
+    AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10,
+    NOVEMBER = 11, DECEMBER = 12;
+}
---
+++ src/org/javaturk/oofp/ch03/Size.java
@@ -0,0 +1,13 @@
+package org.javaturk.oofp.ch03;
+
+import org.javaturk.oofp.ch03.extending.AnInterface;
+
+public enum Size implements AnInterface{
+
+	SMALL, MEDIUM, LARGE, EXTRA_LARGE;
+
+	@Override
+	public void doThis() {
+		
+	}
+}
